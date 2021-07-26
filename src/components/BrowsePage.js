cd7ce7bf27f39c8369fde84fc1890d558b2ebd9d
rkshockey <rkshockey@gmail.com>
2021-07-26 16:03:52 -0400

    Created functions for search bar

--- src/components/BrowsePage.js
+++ src/components/BrowsePage.js
@@ -1,19 +1,36 @@
 import './BrowsePage.css';
 import Search from './Search'
 import Browse from './Browse'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { type } from 'language-tags';
 
 function BrowsePage() {
   const [search, setSearch] = useState({type: '', name: ''})
+  const [items, setItems] = useState([])
+  const [selectedItems, setSelectedItems] = useState([])
 
   function changeSearch(key, value){
     setSearch({...search, [key]: value})
   }
 
+  function selection (type, name){
+    const select = items.filter(item => item.name.includes(name))
+    if (type === ''){
+        setSelectedItems(select)
+    }else{
+        setSelectedItems(select.filter(item => item.name.includes(name)))
+    }
+  }
+
+  useEffect(() => {
+    const { type, name } = search
+    selection(type, name)
+  }, [search])
+
   return (
     <div className="App">
       <Search search={search} change={changeSearch} />
-      <Browse search={search} />
+      <Browse selected={selectedItems} />
     </div>
   );
 }
