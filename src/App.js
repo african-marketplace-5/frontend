f42e9b65803fd2f99e0e44ad8f0ab76baba2b581
garrett <yien.garrett94@gmail.com>
2021-07-27 13:13:54 -0500

    built preliminary login form, signup form, cleaned up routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import React from 'react'
+import React from 'react';
 import { Route, Switch } from "react-router-dom";
-import Header from './components/Header'
-import Home from './components/Home'
+import Header from './components/Header';
+import Home from './components/Home';
+import Signup from './components/userportal/signup';
+import Login from './components/userportal/login';
 
 function App() {
   return (
@@ -11,13 +13,18 @@ function App() {
 
       <Switch>
       
-      <Route exact path="/">
-        <Home />
-      </Route>
-
-    </Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path='/signup' component={Signup}>
+          <Signup/>
+        </Route>
+        <Route path='/login' component={Login}>
+          <Login/>
+        </Route>
+      </Switch>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/userportal/validation/userFormSchema.js
@@ -0,0 +1,22 @@
+import * as yup from 'yup'
+
+const userformSchema = yup.object().shape({
+    username: yup
+    .string()
+    .trim()
+    .required('Username is required'),
+    password: yup
+    .string()
+    .trim()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters long')
+    .max(16, 'Password cannot be longer than 16 characters'),
+    location_id: yup
+    .string()
+    .trim(),
+    tos: yup.boolean()
+    .required('The Terms of Service must be accepted')
+    .oneOf([true], 'The Terms of Service must be accepted')
+});
+
+export default userformSchema;
